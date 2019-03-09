83acdf7c3607128e4a431a2f3a14df15e346669c
Manu Arora <mannuarora700@gmail.com>
2019-03-10 03:50:34 +0530

    DS++ prepend() append() printlist() Node class

--- data structures and algorithms/LinkedList/linkedList.js
+++ data structures and algorithms/LinkedList/linkedList.js
@@ -1,6 +1,13 @@
 // Implement Linked List with all the necessary methods 
 // Use objects for referencing. Earlier I used arrays, this time objects. Because why not.
+// Implement Node class too which contains head and next values only
 
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
 class LinkedList {
 	constructor(value) {
 		// Set head object
@@ -12,16 +19,43 @@ class LinkedList {
 		this.length = 1; // Initial length
 	}
     append(value) {
-      const newNode = {
-        value: value,
-        next: null
-      };
+      const newNode = new Node(value);
       this.tail.next = newNode;
       this.tail = newNode;
       this.length++;
     }
+    prepend(value) {
+    	const newNode = new Node(value);
+    	newNode.next = this.head;
+    	this.head = newNode;
+    	this.length++;
+    }
+ 	printList() {
+    	const array = [];
+    	let currentNode = this.head;
+    	while(currentNode !== null) {
+    		array.push(currentNode.value);
+    		currentNode = currentNode.next;
+    	}
+    	return array;
+    }
+    // Display with arrows because I love it. kbye.
+    printListPretty() {
+    	const array = [];
+    	let currentNode = this.head;
+    	while(currentNode !== null) {
+    		array.push(currentNode.value);
+    		currentNode = currentNode.next;
+    	}
+    	return array.join('->');
+    }
+
+    // insert(index, value) {
+
+    // }
 }
 const ll = new LinkedList(10);
 ll.append(5);
 ll.append(16);
-console.log(ll);
+ll.prepend(1);
+ll.printList();
