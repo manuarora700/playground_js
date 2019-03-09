355d3d76cd85419cb74e0c9e6715e20589589b57
Manu Arora <mannuarora700@gmail.com>
2019-03-10 00:32:18 +0530

    DS++ brute force for

--- data structures and algorithms/FirstRecurringCharacter/firstRecurringCharacter.js
+++ data structures and algorithms/FirstRecurringCharacter/firstRecurringCharacter.js
@@ -1,3 +1,17 @@
+// Brute force O(n^2)
+function firstRecurringCharacter(arr) {
+  let map = {};
+
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i+1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) return arr[i];
+    }
+  }
+  return undefined; // No match
+}
+
+firstRecurringCharacter([1,2,3,4,5,6,7,6,7,8,4,3,5]);
+
 // O(n) approach using Hash Table (Objects)
 
 function firstRecurringCharacter(arr) {
@@ -9,7 +23,7 @@ function firstRecurringCharacter(arr) {
     }
     map[arr[i]] = 1;
   }
-
+  return undefined; // No match
 }
 
 firstRecurringCharacter([1,2,3,4,5,6,7,6,7,8,4,3,5]);
