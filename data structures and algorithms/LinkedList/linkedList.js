1337b598f8e3d1baebab8d363df23a14541e21c5
Manu Arora <mannuarora700@gmail.com>
2019-03-10 03:33:16 +0530

    DS++

--- data structures and algorithms/LinkedList/linkedList.js
+++ data structures and algorithms/LinkedList/linkedList.js
@@ -11,6 +11,17 @@ class LinkedList {
 		this.tail = this.head; // keep track of tail
 		this.length = 1; // Initial length
 	}
+    append(value) {
+      const newNode = {
+        value: value,
+        next: null
+      };
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
 }
-
 const ll = new LinkedList(10);
+ll.append(5);
+ll.append(16);
+console.log(ll);
