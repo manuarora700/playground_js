e8a97e749cf38e67ef62f2ee9d77cc855ba6f17f
Manu Arora <mannuarora700@gmail.com>
2019-03-16 23:01:04 +0530

    Leetcode++ Queue using 2 stacks. wow. ;p

---
+++ Leetcode/queueFromStacks.js
@@ -0,0 +1,63 @@
+// 232. Implement Queue using Stacks
+// Implement the following operations of a queue using stacks.
+
+// push(x) -- Push element x to the back of queue.
+// pop() -- Removes the element from in front of queue.
+// peek() -- Get the front element.
+// empty() -- Return whether the queue is empty.
+// Example:
+
+// MyQueue queue = new MyQueue();
+
+// queue.push(1);
+// queue.push(2);  
+// queue.peek();  // returns 1
+// queue.pop();   // returns 1
+// queue.empty(); // returns false
+// Notes:
+
+// You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+// Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+// You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
