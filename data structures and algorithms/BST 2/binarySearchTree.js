8fd0090d12a7a850eeeadd4c3d10870215dada36
Manu Arora <mannuarora700@gmail.com>
2019-03-20 03:06:17 +0530

    DS++ BST

--- data structures and algorithms/BST 2/binarySearchTree.js
+++ data structures and algorithms/BST 2/binarySearchTree.js
@@ -1,10 +1,10 @@
 // Implement a BST with Node class and BST class. Implement insert(), lookup() and delete() methods.
-
 class Node {
   constructor(value) {
-    this.value = value;
     this.left = null;
     this.right = null;
+    this.value = value;
+
   }
 }
 
@@ -13,22 +13,58 @@ class BST {
     this.root = null;
   }
 
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if(this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if(value < currentNode.value) {
+          //LEFT SUBTREE
+          if(!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        }
+        else {
+          // RIGHT SUBTREE
+          if(!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
   lookup(value) {}
   remove(value) {}
 }
 
 
-const bst = new BST();
+const tree = new BST();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+//     9
+//  4     20
+//1  6  15  170
 
 
 // Traversal function to traverse a BST
 
 function traverse(node) {
   const tree = { value: node.value };
-
-  tree.left = node.left === null ? null : traverse(tree.left);
-  tree.right = node.right === null ? null : traverse(tree.right);
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
   return tree;
-  
+
 }
