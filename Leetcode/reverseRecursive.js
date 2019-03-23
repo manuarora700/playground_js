38252edb2dee0e70ffe3f6d0cbc4cc61982b4356
Manu Arora <mannuarora700@gmail.com>
2019-03-23 21:51:57 +0530

    Recursively reverse a string

--- Leetcode/reverseRecursive.js
+++ Leetcode/reverseRecursive.js
@@ -3,6 +3,11 @@
 
 function stringReverseRecursively(str) {
 	// code here
+	if (str === "") {
+    return "";
+  	} else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
 }
 
-stringReverseRecursively("Hello Paaji")
+stringReverseRecursively("Hello Paaji");
