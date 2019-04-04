616100df6b8c7e723dd79be96d7dfa28631263e7
Manu Arora <mannuarora700@gmail.com>
2019-04-04 21:32:19 +0530

    Leetcode++ ExcelToNumberConversion

--- Leetcode/excelSheetToNumber.js
+++ Leetcode/excelSheetToNumber.js
@@ -33,3 +33,17 @@
 // Input: "ZY"
 // Output: 701
 
+var titleToNumber = function(s) {
+    const charCodeBase = 'A'.charCodeAt(0) - 1;
+    const n = s.length;
+    let number = 0;
+
+    /* 
+     * Think of it as base 26. For example,
+     * Column number of "AB" = 1 * 26^1 + 2 * 26^0 
+     */
+    for (let i = 0; i < n; i++)
+        number += (s.charCodeAt(i) - charCodeBase) * Math.pow(26, n-i-1);
+    
+    return number;
+};
