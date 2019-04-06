1b3d6b30f122fdfd5cf00a16e3d5d446fb7b2abb
Manu Arora <mannuarora700@gmail.com>
2019-04-06 23:39:25 +0530

    Leetcode++ best time to buy and sell stock one pass solution o(n)

--- Leetcode/bestTimeToBuyAndSellStockI.js
+++ Leetcode/bestTimeToBuyAndSellStockI.js
@@ -44,3 +44,23 @@ var maxProfit = function(prices) {
   }
   return maxprofit;
 };
+
+// Using one pass solution -- BAHUT JADA TEZ SOLUTION O(N)
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minprice = Infinity;
+    let maxprofit = 0;
+    
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i] < minprice) {
+            minprice = prices[i];
+        } else if(prices[i] -  minprice > maxprofit) {
+            maxprofit = prices[i] - minprice;
+        }
+    }
+    return maxprofit;
+};
