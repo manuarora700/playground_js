4b56c11f5919e153cbe7e525946f7f818d717ce4
Manu Arora <mannuarora700@gmail.com>
2019-06-11 20:13:11 +0530

    Leetcode++ sorted

---
+++ Leetcode/squaresSorted.js
@@ -0,0 +1,34 @@
+/**
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Note:
+
+1 <= A.length <= 10000
+-10000 <= A[i] <= 10000
+A is sorted in non-decreasing order.
+ */
+var sortedSquares = function(A) {
+    let squares = [];
+    
+    for(let i = 0; i < A.length; i++) {
+        squares.push(A[i]*A[i]);
+    }
+    
+    squares.sort(function(a,b){
+        return a-b;
+    });
+    
+    return squares;
+};
