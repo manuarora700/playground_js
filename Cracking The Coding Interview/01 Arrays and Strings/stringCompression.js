2b82494f965b98e7e02da1b5753b403dcde7ad2b
Manu Arora <mannuarora700@gmail.com>
2019-06-18 03:08:40 +0530

    Minor modifications in stringCompression

--- Cracking The Coding Interview/01 Arrays and Strings/stringCompression.js
+++ Cracking The Coding Interview/01 Arrays and Strings/stringCompression.js
@@ -3,7 +3,7 @@ Approach 1 - Check the next value; if same then increase counter and keep iterat
 to the result and reset counter.
 Fairly Straightforward.
 
-Time - O(S)
+Time - O(S + N^2) Because string concatenation operates on O(N^2) runtime.
 Space - O(1)
 */
 
@@ -11,19 +11,20 @@ function stringCompression(string) {
   string = string.replace(/\s/g, '');
 
   let counter = 1;
-  let result = "";
+  let compressedString = "";
   let j = 1;
   for(let i = 0; i < string.length; i++) {
     if(string[i] === string[j]) {
       counter++;
       j++;
     } else {
-      result += string[i] + counter;
+      compressedString += string[i] + counter;
       counter = 1;
       j++;
     }
   }
-  return result;
+  // if compressed string is not smaller than original string then return original string
+  return compressedString.length < string.length ? compressedString : string;
 
 }
 
