e12da6f3b3c501d26fc8ae2c04725e85f7dd81a6
Manu Arora <mannuarora700@gmail.com>
2019-06-26 03:38:49 +0530

    ampm hackerrank++

---
+++ Hackerrank/[Track] Algorithms/ampm.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the timeConversion function below.
+ */
+function timeConversion(s) {
+    let AMPM = s.slice(-2);
+    let timeArr = s.slice(0, -2).split(":");
+
+    if (AMPM === "AM" && timeArr[0] === "12") {
+        // catching edge-case of 12AM
+        timeArr[0] = "00";
+    } else if (AMPM === "PM") {
+        // everything with PM can just be mod'd and added with 12 - the max will be 23
+        timeArr[0] = (timeArr[0] % 12) + 12
+    }
+
+    return timeArr.join(":");
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = timeConversion(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
