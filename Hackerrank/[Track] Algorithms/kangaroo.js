b4748f927abc63543e98b3af83f7e9bfc0e51772
Manu Arora <mannuarora700@gmail.com>
2019-06-27 03:24:16 +0530

    Hackerrank++ kangaroo

---
+++ Hackerrank/[Track] Algorithms/kangaroo.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the kangaroo function below.
+function kangaroo(x1, v1, x2, v2) {
+
+    let d = x2 - x1;
+
+    while (d) {
+        x1 = x1 + v1;
+        x2 = x2 + v2;
+
+        let dnew = x2 - x1;
+        if (dnew === 0) return "YES";
+
+        if (dnew >= d || d < -1) // Meaning distance is increasing, they'll never meet
+        {
+            return "NO"; 
+        }
+        d = dnew;
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const x1V1X2V2 = readLine().split(' ');
+
+    const x1 = parseInt(x1V1X2V2[0], 10);
+
+    const v1 = parseInt(x1V1X2V2[1], 10);
+
+    const x2 = parseInt(x1V1X2V2[2], 10);
+
+    const v2 = parseInt(x1V1X2V2[3], 10);
+
+    let result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
