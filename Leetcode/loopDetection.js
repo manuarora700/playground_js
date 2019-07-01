558401b4e5d0f0186e8fceaa5fd0961f6ec4df00
Manu Arora <mannuarora700@gmail.com>
2019-07-01 17:17:23 +0530

    Leetcode++ loop detection

---
+++ Leetcode/loopDetection.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head === null || head.next === null) return false;
+    
+    let fast = head;
+    let slow = head;
+    
+    while(fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if(fast === slow) return true;
+    }
+    return false;
+    
+};
